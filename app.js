94502efc558417d432ed2fc014aae6545e133bb6
franciSauce <careyfrancisowino@gmail.com>
2025-10-02 08:59:31 +0300

    Add node

---
+++ app.js
@@ -0,0 +1,20 @@
+// Node factory function
+const node = (value = null) => {
+    return {
+        value,
+        nextNode: null
+    };
+};
+
+// LinkedList factory function
+const linkedList = () => {
+    return {
+        
+    }
+}
+
+
+export {
+    node,
+    linkedList
+};
