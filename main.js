c98e949840fe50321b81cc399ade77eabdb6307e
franciSauce <careyfrancisowino@gmail.com>
2025-10-02 10:11:11 +0300

    Import node() & linkedList()

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
+import { node, linkedList } from "./app.js";
 
+const list = linkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
