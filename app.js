42cb6cbf15084fa55630b79bde3993e239688efa
franciSauce <careyfrancisowino@gmail.com>
2025-10-02 09:22:43 +0300

    Add head(), tail(), at() & pop()

--- app.js
+++ app.js
@@ -46,7 +46,56 @@ const linkedList = () => {
             return count;
         },
 
+        head() {
+            return this.head;
+        },
+
+        tail() {
+            if (!this.head) return null;
+
+            let current = this.head;
+            while (current.nextNode) {
+                current = current.nextNode;
+            }
+
+            return current;
+        },
+
+        at(index) {
+            if (index < 0 || !this.head) return null;
+
+            let current = this.head;
+            let count = 0;
+
+            while (current && count < index) {
+                current = current.nextNode;
+                count++;
+            }
+
+            return current || null;
+        },
+
+        pop() {
+            if (!this.head) return;
+
+            if (!this.head.nextNode) {
+                this.head = null;
+                return;
+            }
+
+            let current = this.head;
+            let previous = null;
+
+            while (current.nextNode) {
+                previous = current;
+                current = current.nextNode;
+            }
+
+            previous.nextNode = null;
+        },
         
+
+
     }
 }
 
