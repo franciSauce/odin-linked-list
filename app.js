f1f72576421538a8303e6f2d600a32c6f80e310e
franciSauce <careyfrancisowino@gmail.com>
2025-10-02 09:12:45 +0300

    Add append(), prepend() & size()

--- app.js
+++ app.js
@@ -9,6 +9,43 @@ const node = (value = null) => {
 // LinkedList factory function
 const linkedList = () => {
     return {
+        head: null,
+
+        // functions
+        append(value) {
+            const newNode = node(value);
+
+            if (!this.head) {
+                this.head = newNode;
+                return;
+            }
+
+            let current = this.head;
+            while (current.nextNode) {
+                current = current.nextNode;
+            }
+
+            current.nextNode = newNode;
+        },
+
+        prepend(value) {
+            const newNode = node(value);
+            newNode.nextNode = this.head;
+            this.head = newNode;
+        },
+
+        size() {
+            let count =0;
+            let current = this.head;
+
+            while (current) {
+                count++;
+                current = current.nextNode;
+            }
+
+            return count;
+        },
+
         
     }
 }
